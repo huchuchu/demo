da23b88001aa89b78f3fa5117b7f1958364327af
huchuchu <huchuchu.yoo@gmail.com>
2022-06-19 01:33:50 +0900

    MVVMstart

--- src/main/webapp/app/view/window/UpdatePasswordController.js
+++ src/main/webapp/app/view/window/UpdatePasswordController.js
@@ -2,11 +2,11 @@ Ext.define('Study.view.window.UpdatePasswordController',{
     extend : 'Ext.app.ViewController',
     alias : 'controller.updatePassword',
     changeBtn : function(btn){
-        btn.up(window).close();
+        btn.up("window").close();
 
     },
     closeBtn : function(btn){
-        btn.up(window).close();
+        btn.up("window").close();
     }
 
 });
