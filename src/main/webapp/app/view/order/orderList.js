2468488b5a37aa07bd750614501d3a2df797911f
huchuchu <huchuchu.yoo@gmail.com>
2022-06-18 19:40:46 +0900

    addGrid

--- src/main/webapp/app/view/order/orderList.js
+++ src/main/webapp/app/view/order/orderList.js
@@ -1,9 +1,71 @@
  Ext.define('Study.view.product.orderList', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.grid.Panel',
     xtype: 'orderList',
-    width: 500,
-    height: 500,
-    html : 'order list',
+    title : '주문목록',
+    columnLines : true,
+    tbar : [{
+        xtype : 'combo',
+        displayField : 'a',
+        valueField : 'b',
+        store : {
+            fields : ['a', 'b'],
+            data : [{
+                a : 'a',
+                b : 'b'
+            }]
+        }
+    },{
+        xtype : 'textfield',
+        emptyText : '검색어를 입력하세요'
+    },{
+        xtype : 'button',
+        text : '검색'
+    }],
+    columns :[{
+        xtype : 'rownumberer'
+    },{
+        text : '상품명',
+        dataIndex : 'productNm',
+        flex : 1
+    },{
+        text : '가격',
+        dataIndex : 'price',
+        flex : 1
+    },{
+        text : '재고량',
+        dataIndex : 'amount',
+        flex : 1
+    },{
+        text : '등록일',
+        dataIndex : 'rgstrDt',
+        flex : 1
+    }
+    ],
+    store : {
+        fields : ['productNm', 'price', 'amount', 'rgstrDt'],
+        data : [{
+            productNm : '지우개',
+            price : 500,
+            amount : 100,
+            rgstrDt : new Date()
+        },{
+            productNm : '지우개',
+            price : 500,
+            amount : 100,
+            rgstrDt : new Date()
+        },{
+            productNm : '지우개',
+            price : 500,
+            amount : 100,
+            rgstrDt : new Date()
+        },{
+            productNm : '지우개',
+            price : 500,
+            amount : 100,
+            rgstrDt : new Date()
+        }
+        ]
+    }
 
   
 });
--- src/main/webapp/app/view/product/productList.js
+++ src/main/webapp/app/view/product/productList.js
@@ -1,9 +1,82 @@
  Ext.define('Study.view.product.productList', {
     extend: 'Ext.panel.Panel',
     xtype: 'productList',
-    width: 500,
-    height: 500,
-    html : 'product list',
+    title : '상품목록',
+    items : [{
+        xtype : 'toolbar',
+        items : [{
+            xtype : 'textfield',
+            fieldLabel  : '상품명'
+        },{
+            xtype : 'numberfield',
+            fieldLabel  : '가격'
 
-  
+        },{
+            xtype : 'numberfield',
+            fieldLabel  : '재고량'
+
+        },{
+            xtype : 'button',
+            text : '등록'
+        }
+        ]
+    },{
+        xtype : 'grid',
+        columnLines : true,
+        tbar : [{
+            xtype : 'textfield',
+            emptyText : '검색어를 입력하세요'
+        },{
+            xtype : 'button',
+            text : '검색'
+        }],
+        columns :[{
+            xtype : 'rownumberer'
+        },{
+            text : '상품명',
+            dataIndex : 'productNm',
+            flex : 1
+        },{
+            text : '가격',
+            dataIndex : 'price',
+            flex : 1
+        },{
+            text : '재고량',
+            dataIndex : 'amount',
+            flex : 1
+        },{
+            text : '등록일',
+            dataIndex : 'rgstrDt',
+            flex : 1
+        }
+        ],
+        store : {
+            fields : ['productNm', 'price', 'amount', 'rgstrDt'],
+            data : [{
+                productNm : '지우개',
+                price : 500,
+                amount : 100,
+                rgstrDt : new Date()
+            },{
+                productNm : '지우개',
+                price : 500,
+                amount : 100,
+                rgstrDt : new Date()
+            },{
+                productNm : '지우개',
+                price : 500,
+                amount : 100,
+                rgstrDt : new Date()
+            },{
+                productNm : '지우개',
+                price : 500,
+                amount : 100,
+                rgstrDt : new Date()
+            }
+            ]
+        }
+
+    }], 
+
+
 });
